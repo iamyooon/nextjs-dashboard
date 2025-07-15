730e4824e57a11e554c06d7c9207d3cda3ec5960
seokhoon.yoon <iamyooon@gmail.com>
2025-07-15 10:05:45 +0900

    chapter 15

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,8 +1,11 @@
 'use server';
- import { z } from 'zod';
- import { revalidatePath } from 'next/cache';
- import { redirect } from 'next/navigation';
- import postgres from 'postgres';
+import { z } from 'zod';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+import postgres from 'postgres';
+ 
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
  
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
  
@@ -97,4 +100,24 @@ export async function updateInvoice(id: string, formData: FormData) {
 export async function deleteInvoice(id: string) {
   await sql`DELETE FROM invoices WHERE id = ${id}`;
   revalidatePath('/dashboard/invoices');
+}
+
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
 }
